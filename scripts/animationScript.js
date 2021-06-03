019177511fc36adeb6be4cb4aaf3a6d3cb08be11
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-03 18:51:14 -0500

    Animation updated

---
+++ scripts/animationScript.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+        // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
+})
