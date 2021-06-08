c8c7d3f0b835166a261486df31b4a96e952acdd0
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-07 20:23:34 -0500

    Changed: Intro, About and Skills

---
+++ scripts/animationScriptSkills.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-animation-skills'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json'
+    path: 'data.json'
+})
