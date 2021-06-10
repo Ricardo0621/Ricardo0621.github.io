2798b55eead8a5bbe518df1973f63630e16a7ba4
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-10 18:14:38 -0500

    Changed animations file

--- scripts/animationScriptSkills.js
+++ scripts/animationScriptSkills.js
@@ -3,7 +3,7 @@ var animation = bodymovin.loadAnimation({
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'data.json'
+    path: 'animations/animation-skills.json'
         // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json'
         // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
         // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json'
