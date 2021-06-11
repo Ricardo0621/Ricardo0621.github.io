9eea9d7a478481ffb3d062033951c2754b14fc8e
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-10 19:06:53 -0500

    :))

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -3,9 +3,9 @@ var animation = bodymovin.loadAnimation({
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    // path: 'animations/animation.json'
-    // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
-    // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
-    // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
-    path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
+    path: 'animations/animation.json'
+        // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
+        // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
+        // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
+        // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
 })
