558b68857c0d7b99a92d199e56a42125ea858593
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-15 23:06:47 -0500

    Added on scroll text animation

--- scripts/fadeInEffect.js
+++ scripts/fadeInEffect.js
@@ -1,7 +1,12 @@
 $(document).on("scroll", function() {
     var pageTop = $(document).scrollTop();
     var pageBottom = pageTop + $(window).height();
-    var tags = $(".section2 .about-section");
+    var tags = [$(".section2 .about-wrapper"),
+        $(".section1 .skills-wrapper"),
+        $(".section2 .projects-wrapper"),
+        $(".section1 .projects-wrapper"),
+        $(".section2 #contact-form"),
+    ];
 
     for (var i = 0; i < tags.length; i++) {
         var tag = tags[i];
