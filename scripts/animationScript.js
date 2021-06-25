1a4103100fbad2698d8d6627031fcfe69f8c24e6
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-25 13:45:33 -0500

    O.o

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -1,17 +1,24 @@
-const lottieAnimation = document.getElementById('lottie-animation')
-
+var lottieAnimation = document.getElementById('lottie-animation');
+var state = 'play';
 var animation = bodymovin.loadAnimation({
     container: lottieAnimation,
     renderer: 'svg',
     loop: false,
     autoplay: false,
-    // path: 'animations/animation.json',
-    path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
+    path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json'
+        // path: 'animations/animation.json',
+        // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
         // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
         // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
         // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
 })
-lottieAnimation.addEventListener('click', (e) => {
-    animation.play();
-    console.log('Playing');
+animation.goToAndStop(14, true);
+lottieAnimation.addEventListener('click', () => {
+    if (state === 'play') {
+        animation.playSegments([14, 27], true);
+        state = 'pause';
+    } else {
+        animation.playSegments([0, 14], true);
+        state = 'play';
+    }
 });
