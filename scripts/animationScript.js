9578f8a1d0c2ebd91e830dadd1ff917f915af3a4
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-25 13:10:41 -0500

    Added animation json file

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -5,11 +5,11 @@ var animation = bodymovin.loadAnimation({
     renderer: 'svg',
     loop: false,
     autoplay: false,
-    // path: 'animations/animation.json',
+    path: 'animations/animation.json',
     // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
     // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
     // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
-    path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
+    // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
 })
 lottieAnimation.addEventListener('enterFrame', function() {
     animation.play();
