27a9ce12c34d352d8802e548dfbe090428e26d46
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-25 13:25:07 -0500

    Playing animation on click

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -1,22 +1,17 @@
 const lottieAnimation = document.getElementById('lottie-animation')
 
 var animation = bodymovin.loadAnimation({
-        container: lottieAnimation,
-        renderer: 'svg',
-        loop: false,
-        autoplay: false,
-        path: 'animations/animation.json',
-        // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
-        // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
-        // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
-        // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
-    })
-    // lottieAnimation.addEventListener('enterFrame', (e) => {
-    //     animation.play();
-    //     console.log('Playing');
-    // });
-var animationStart = 0;
-$(window).scroll(function() {
-    animation.playSegments([animationStart, animationStart + 1], true);
-    animationStart++;
+    container: lottieAnimation,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'animations/animation.json',
+    // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
+    // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
+    // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
+    // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
 })
+lottieAnimation.addEventListener('click', (e) => {
+    animation.play();
+    console.log('Playing');
+});
