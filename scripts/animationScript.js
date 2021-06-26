df2dd8f824893e50bebb7100b40dfb4d7e3e0956
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-26 16:41:52 -0500

    Testing bodymovin vs lottie-player performance

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -1,24 +1,24 @@
-const container = document.getElementById('lottie-animation');
+// const container = document.getElementById('lottie-animation');
 const frameToStop = 154;
-var animation = bodymovin.loadAnimation({
-    container: container,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    rendererSettings: {
-        progressiveLoad: true,
-    },
-    // path: 'https://assets2.lottiefiles.com/packages/lf20_i9mxcD.json'
-    path: 'animations/animation.json',
-    // path: 'https://assets.codepen.io/4358584/01.json',
-    // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
-    // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
-    // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
-    // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
-})
+// var animation = bodymovin.loadAnimation({
+//     container: container,
+//     renderer: 'svg',
+//     loop: false,
+//     autoplay: false,
+//     rendererSettings: {
+//         progressiveLoad: true,
+//     },
+//     // path: 'https://assets2.lottiefiles.com/packages/lf20_i9mxcD.json'
+//     path: 'animations/animation.json',
+//     // path: 'https://assets.codepen.io/4358584/01.json',
+//     // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
+//     // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
+//     // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
+//     // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
+// })
 LottieInteractivity.create({
     mode: 'scroll',
-    player: animation,
+    player: '#lottie-animation',
     actions: [{
         visibility: [0, 1],
         type: 'seek',
