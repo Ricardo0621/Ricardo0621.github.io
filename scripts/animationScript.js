f0323996ca8727e7222680b059e6ea209284a0b2
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-25 19:34:29 -0500

    Changed animations

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -1,24 +1,31 @@
-var lottieAnimation = document.getElementById('lottie-animation');
-var state = 'play';
+const container = document.getElementById('lottie-animation');
+const frameToStop = 154;
 var animation = bodymovin.loadAnimation({
-    container: lottieAnimation,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json'
-        // path: 'animations/animation.json',
+        container: container,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        rendererSettings: {
+            progressiveLoad: true,
+        },
+        path: 'animations/animation.json',
+        // path: 'https://assets.codepen.io/4358584/01.json',
         // path: 'https://assets10.lottiefiles.com/packages/lf20_44gjzh9a.json' //Typing in floor broken background
         // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json' //Dude typing
         // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
         // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json' //Programming
-})
-animation.goToAndStop(14, true);
-lottieAnimation.addEventListener('click', () => {
-    if (state === 'play') {
-        animation.playSegments([14, 27], true);
-        state = 'pause';
-    } else {
-        animation.playSegments([0, 14], true);
-        state = 'play';
+    })
+    // container.addEventListener('enterFrame', () => {
+    //     console.log('enterFrame', animation.currentFrame);
+    // });
+animation.addEventListener('DOMLoaded', function() {
+    animation.addEventListener('enterFrame', repeat);
+    animation.play();
+
+    function repeat() {
+        if (animation.currentRawFrame > frameToStop) {
+            animation.addEventListener('enterFrame', repeat);
+            animation.goToAndPlay(0, true);
+        }
     }
-});
+})
--- scripts/animationScriptSkills.js
+++ scripts/animationScriptSkills.js
@@ -1,10 +1,18 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('lottie-animation-skills'),
+const lottieAnimation = document.getElementById('lottie-animation-skills');
+var lottie_animation = bodymovin.loadAnimation({
+    container: lottieAnimation,
     renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'animations/animation-skills.json'
-        // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json'
-        // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
-        // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json'
+    loop: false,
+    autoplay: false,
+    rendererSettings: {
+        progressiveLoad: true,
+    },
+    // path: 'https://assets.codepen.io/4358584/01.json',
+    // path: 'animations/animation-skills.json'
+    // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json'
+    // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
+    path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json'
 })
+lottieAnimation.addEventListener('enterFrame', () => {
+    lottie_animation.playSegments([0, 50], true);
+});
