df308a8a0b1b7c95947dc4d327ebf7259f2e3a79
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2021-06-26 17:09:10 -0500

    Back to Bodymovin

--- scripts/animationScriptSkills.js
+++ scripts/animationScriptSkills.js
@@ -1,22 +1,22 @@
-// const lottieAnimation = document.getElementById('lottie-animation-skills');
+const lottieAnimation = document.getElementById('lottie-animation-skills');
 const number_of_frames = 130;
-// var lottie_animation = bodymovin.loadAnimation({
-//     container: lottieAnimation,
-//     renderer: 'svg',
-//     loop: false,
-//     autoplay: false,
-//     rendererSettings: {
-//         progressiveLoad: true,
-//     },
-//     // path: 'https://assets.codepen.io/4358584/01.json',
-//     path: 'animations/animation-skills.json'
-//         // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json'
-//         // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
-//         // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json'
-// })
+var lottie_animation = bodymovin.loadAnimation({
+    container: lottieAnimation,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    rendererSettings: {
+        progressiveLoad: true,
+    },
+    // path: 'https://assets.codepen.io/4358584/01.json',
+    path: 'animations/animation-skills.json'
+        // path: 'https://assets1.lottiefiles.com/packages/lf20_jaejabqz.json'
+        // path: 'https://assets10.lottiefiles.com/packages/lf20_lln7m43m.json'
+        // path: 'https://assets10.lottiefiles.com/packages/lf20_xsnsvpbs.json'
+})
 LottieInteractivity.create({
     mode: 'scroll',
-    player: '#lottie-animation-skills',
+    player: lottie_animation,
     actions: [{
         visibility: [0, 1],
         type: 'seek',
